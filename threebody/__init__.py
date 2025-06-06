9c40ab593ebad666d60f3f688cd909b443f1e044
Pickle <62830510+ArashiWander@users.noreply.github.com>
2025-06-06 18:17:39 +0100

    Vectorize acceleration calculation

--- threebody/__init__.py
+++ threebody/__init__.py
@@ -1,12 +1,14 @@
 """Three-body simulation utilities."""
 
 from .physics import Body, perform_rk4_step, system_energy
+from .integrators import compute_accelerations
 from .constants import G_REAL, SPACE_SCALE, SOFTENING_FACTOR_SQ
 
 __all__ = [
     'Body',
     'perform_rk4_step',
     'system_energy',
+    'compute_accelerations',
     'G_REAL',
     'SPACE_SCALE',
     'SOFTENING_FACTOR_SQ',
